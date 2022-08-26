7e7bada3b9a595fd24771bf64a31f42141c56cdc
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-08-26 18:51:52 +0200

    initial revision

---
+++ src/main/java/org/example/tautologybackend/TautologyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.tautologybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TautologyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TautologyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tautologybackend/TautologyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tautologybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TautologyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
