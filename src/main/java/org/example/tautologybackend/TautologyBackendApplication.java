6ecc5e8bab496540d66341dab71566d12922cb2d
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-09-04 23:47:31 +0200

    add annotation to fis springfox error

--- src/main/java/org/example/tautologybackend/TautologyBackendApplication.java
+++ src/main/java/org/example/tautologybackend/TautologyBackendApplication.java
@@ -2,8 +2,10 @@ package org.example.tautologybackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc // to fix issue with springfox error
 public class TautologyBackendApplication {
 
     public static void main(String[] args) {
