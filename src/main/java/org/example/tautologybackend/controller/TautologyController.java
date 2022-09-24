3874aa043dc32c29e79256b93267a2ccce8d7e8c
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2022-09-24 21:47:57 +0200

    allow cross origin

--- src/main/java/org/example/tautologybackend/controller/TautologyController.java
+++ src/main/java/org/example/tautologybackend/controller/TautologyController.java
@@ -8,12 +8,14 @@ import org.example.tautology.openapi.api.TautologyApi;
 import org.example.tautology.openapi.model.*;
 import org.example.tautologybackend.service.TautologyService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @Slf4j
 @RestController
+@CrossOrigin
 public class TautologyController implements TautologyApi {
 
     private final TautologyService service;
